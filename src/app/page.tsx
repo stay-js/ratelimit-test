135839f7dabb1979d31bbab70f5bb29422365b5e
Zétény Nagy <nagyzeteny6@gmail.com>
2023-06-01 19:32:40 +0200

    file names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,17 @@ export const metadata = createMetadata({
   description: 'Upstash Rate Limiter test',
 });
 
-const Page: NextPage = () => {
-  return (
-    <main className="mx-auto flex max-w-2xl flex-col gap-16 px-4 py-12">
-      <div className="flex flex-col gap-4 text-center">
-        <h1 className="text-5xl font-bold">Upstash Rate Limiter test</h1>
-        <h2 className="text-lg">
-          You can send <b>three request</b> per every <b>ten seconds</b>.
-        </h2>
-      </div>
+const Page: NextPage = () => (
+  <main className="mx-auto flex max-w-2xl flex-col gap-16 px-4 py-12">
+    <div className="flex flex-col gap-4 text-center">
+      <h1 className="text-5xl font-bold">Upstash Rate Limiter test</h1>
+      <h2 className="text-lg">
+        You can send <b>three request</b> per every <b>ten seconds</b>.
+      </h2>
+    </div>
 
-      <Request />
-    </main>
-  );
-};
+    <Request />
+  </main>
+);
 
 export default Page;
